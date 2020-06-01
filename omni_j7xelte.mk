74a918bb8d79b1d5f303e1865d885e9e6975ac4a
4u7157 <61901433+4u7157@users.noreply.github.com>
2020-06-01 11:30:04 +0300

    Fix build error on android 9+ manifest

--- omni_j7xelte.mk
+++ omni_j7xelte.mk
@@ -1,8 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Fix build error on android 9+ manifest
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/Image:kernel \
+     $(LOCAL_PATH)/dt.img:dt.img \
+     $(LOCAL_PATH)/dt.img:boot.img
 
 # Device identifier
 PRODUCT_DEVICE := j7xelte
